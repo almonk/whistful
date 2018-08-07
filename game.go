2e1283e22d4cc761289a40cccc636769dc6d18dd
Alasdair Monk <alasdair.monk@gmail.com>
2018-08-08 00:03:07 +0100

    Get input

--- game.go
+++ game.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
 	"time"
 )
+
+import tl "github.com/JoelOtter/termloop"
 
 type player struct {
 	number int
@@ -17,6 +22,10 @@ var numberOfCardsDealtTotal int
 var p1 player
 var p2 player
 
+type MovingText struct {
+	*tl.Text
+}
+
 func newGame() {
 	// var discardedCards []card
 	freshDeck = deck.generate(deck{})
@@ -25,15 +34,30 @@ func newGame() {
 	p2 = player{number: 2}
 
 	// Start the game by dealing 7 cards to each player
-	dealHeadToPlayer(p1, 7)
-	dealHeadToPlayer(p2, 7)
+	// dealHeadToPlayer(p2, 7)
 
-	// dealHeadToPlayer(p1, 1)
-	fmt.Println(p1.hand)
-	playerPlaysATrick(p1, 4)
+	// // dealHeadToPlayer(p1, 1)
+	// fmt.Println(p1.hand)
+	// playerPlaysATrick(p1, getPlayerInput())
+	level := tl.NewBaseLevel(tl.Cell{
+		Bg: tl.ColorGreen,
+		Fg: tl.ColorGreen,
+		Ch: 'v',
+	})
+
+	game := tl.NewGame()
+	game.Screen().SetLevel(level)
+	game.Screen().AddEntity(tl.NewText(0, 0, "WHIST", tl.ColorBlack, tl.ColorYellow))
+	game.Screen().AddEntity(&MovingText{tl.NewText(0, 1, "AN @ALMONK GAME", tl.ColorBlack, tl.ColorGreen)})
+	game.SetDebugOn(true)
+
+	game.Start()
+
+	dealHeadToPlayer(game, p1, 7)
 }
 
-func dealHeadToPlayer(p player, numberOfCardsToDeal int) {
+func dealHeadToPlayer(game *tl.Game, p player, numberOfCardsToDeal int) {
+
 	for i := 0; i < numberOfCardsToDeal; i++ {
 		if numberOfCardsDealtTotal <= 51 {
 			// Add cards from the deck to
@@ -44,6 +68,9 @@ func dealHeadToPlayer(p player, numberOfCardsToDeal int) {
 				p2.hand = append(p1.hand, freshDeck.cards[numberOfCardsDealtTotal])
 				fmt.Println(fmt.Sprintf("Player %d gets %s", p2.number, freshDeck.cards[numberOfCardsDealtTotal].friendlyName()))
 			}
+
+			drawNextCardX := 26 * numberOfCardsDealtTotal
+			game.Screen().AddEntity(tl.NewRectangle(2, 3, drawNextCardX, 12, tl.RgbTo256Color(255, 255, 255)))
 
 			// Increment the number of cards dealt
 			numberOfCardsDealtTotal++
